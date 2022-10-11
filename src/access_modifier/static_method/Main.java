3f3d2159efe7e0feccf306b8b45156e6357ad510
LuuLeThaiSon <llts28497@gmail.com>
2022-10-11 08:23:19 +0700

    done

---
+++ src/access_modifier/static_method/Main.java
@@ -0,0 +1,15 @@
+package access_modifier.static_method;
+
+public class Main {
+    public static void main(String[] args) {
+        Student.change();
+
+        Student student1 = new Student(1, "a");
+        Student student2 = new Student(2, "b");
+        Student student3 = new Student(3, "c");
+
+        student1.display();
+        student2.display();
+        student3.display();
+    }
+}
